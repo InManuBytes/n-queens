6c3ce9910aca4d6e4475d0a9c39ee8d6e2ddd892
Manuela Gomez <mmanu.gomez@gmail.com>
2019-08-21 00:19:04 -0400

    [wip] Fixed private transpose function passes col conflicts

--- src/Board.js
+++ src/Board.js
@@ -3,31 +3,61 @@
 // The only portions you need to work on are the helper functions (below)
 
 (function() {
-
   window.Board = Backbone.Model.extend({
-
-    initialize: function (params) {
+    initialize: function(params) {
       if (_.isUndefined(params) || _.isNull(params)) {
-        console.log('Good guess! But to use the Board() constructor, you must pass it an argument in one of the following formats:');
-        console.log('\t1. An object. To create an empty board of size n:\n\t\t{n: %c<num>%c} - Where %c<num> %cis the dimension of the (empty) board you wish to instantiate\n\t\t%cEXAMPLE: var board = new Board({n:5})', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: grey;');
-        console.log('\t2. An array of arrays (a matrix). To create a populated board of size n:\n\t\t[ [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...] ] - Where each %c<val>%c is whatever value you want at that location on the board\n\t\t%cEXAMPLE: var board = new Board([[1,0,0],[0,1,0],[0,0,1]])', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: blue;', 'color: black;', 'color: grey;');
-      } else if (params.hasOwnProperty('n')) {
-        this.set(makeEmptyMatrix(this.get('n')));
+        console.log(
+          "Good guess! But to use the Board() constructor, you must pass it an argument in one of the following formats:"
+        );
+        console.log(
+          "\t1. An object. To create an empty board of size n:\n\t\t{n: %c<num>%c} - Where %c<num> %cis the dimension of the (empty) board you wish to instantiate\n\t\t%cEXAMPLE: var board = new Board({n:5})",
+          "color: blue;",
+          "color: black;",
+          "color: blue;",
+          "color: black;",
+          "color: grey;"
+        );
+        console.log(
+          "\t2. An array of arrays (a matrix). To create a populated board of size n:\n\t\t[ [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...], [%c<val>%c,%c<val>%c,%c<val>%c...] ] - Where each %c<val>%c is whatever value you want at that location on the board\n\t\t%cEXAMPLE: var board = new Board([[1,0,0],[0,1,0],[0,0,1]])",
+          "color: blue;",
+          "color: black;",
+          "color: blue;",
+          "color: black;",
+          "color: blue;",
+          "color: black;",
+          "color: blue;",
+          "color: black;",
+          "color: blue;",
+          "color: black;",
+          "color: blue;",
+          "color: black;",
+          "color: blue;",
+          "color: black;",
+          "color: blue;",
+          "color: black;",
+          "color: blue;",
+          "color: black;",
+          "color: blue;",
+          "color: black;",
+          "color: grey;"
+        );
+      } else if (params.hasOwnProperty("n")) {
+        this.set(makeEmptyMatrix(this.get("n")));
       } else {
-        this.set('n', params.length);
+        this.set("n", params.length);
       }
     },
 
     // returns each row as an array
     rows: function() {
-      return _(_.range(this.get('n'))).map(function(rowIndex) {
+      return _(_.range(this.get("n"))).map(function(rowIndex) {
         return this.get(rowIndex);
       }, this);
     },
 
     togglePiece: function(rowIndex, colIndex) {
-      this.get(rowIndex)[colIndex] = + !this.get(rowIndex)[colIndex];
-      this.trigger('change');
+      this.get(rowIndex)[colIndex] = +!this.get(rowIndex)[colIndex];
+      this.trigger("change");
     },
 
     _getFirstRowColumnIndexForMajorDiagonalOn: function(rowIndex, colIndex) {
@@ -46,24 +76,33 @@
       return (
         this.hasRowConflictAt(rowIndex) ||
         this.hasColConflictAt(colIndex) ||
-        this.hasMajorDiagonalConflictAt(this._getFirstRowColumnIndexForMajorDiagonalOn(rowIndex, colIndex)) ||
-        this.hasMinorDiagonalConflictAt(this._getFirstRowColumnIndexForMinorDiagonalOn(rowIndex, colIndex))
+        this.hasMajorDiagonalConflictAt(
+          this._getFirstRowColumnIndexForMajorDiagonalOn(rowIndex, colIndex)
+        ) ||
+        this.hasMinorDiagonalConflictAt(
+          this._getFirstRowColumnIndexForMinorDiagonalOn(rowIndex, colIndex)
+        )
       );
     },
 
     hasAnyQueensConflicts: function() {
-      return this.hasAnyRooksConflicts() || this.hasAnyMajorDiagonalConflicts() || this.hasAnyMinorDiagonalConflicts();
+      return (
+        this.hasAnyRooksConflicts() ||
+        this.hasAnyMajorDiagonalConflicts() ||
+        this.hasAnyMinorDiagonalConflicts()
+      );
     },
 
     _isInBounds: function(rowIndex, colIndex) {
       return (
-        0 <= rowIndex && rowIndex < this.get('n') &&
-        0 <= colIndex && colIndex < this.get('n')
+        0 <= rowIndex &&
+        rowIndex < this.get("n") &&
+        0 <= colIndex &&
+        colIndex < this.get("n")
       );
     },
 
-
-/*
+    /*
          _             _     _
      ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
     / __| __/ _` | '__| __| | '_ \ / _ \ '__/ _ (_)
@@ -79,7 +118,7 @@
     // --------------------------------------------------------------
     //
     // test if a specific row on this board contains a conflict
-    
+
     //specs don't test this function
     hasRowConflictAt: function(rowIndex) {
       var _currentRow = this.get(rowIndex); //this is an array
@@ -90,7 +129,7 @@
         }
       }
       if (_ones > 1) {
-        return true
+        return true;
       } else {
         return false;
       }
@@ -102,60 +141,43 @@
       // just need to loop through rowIndexes -> n of them
       var result = false;
       for (let i = 0; i < this.attributes.n; i++) {
-        if (this.hasRowConflictAt(i) === true){
+        if (this.hasRowConflictAt(i) === true) {
           result = true;
         }
       }
       return result;
     },
 
-
-
     // COLUMNS - run from top to bottom
     // --------------------------------------------------------------
     //
     // test if a specific column on this board contains a conflict
-    hasColConflictAt: function(colIndex) {
-     //holds all our columns
 
-      for (var j = 0; j < size; j++) { //goes through each element in the row
-              //columnsArray[j][i] = this[i][j];
-        column.push(wholeBoard[j][colIndex])
+    // returns a transposed board
+    _transBoard: function() {
+      var wholeBoard = this.rows();
+      var size = this.attributes.n;
+      var columnsArray = [];
+      for (var i = 0; i < size; i++) {
+        var column = [];
+        for (var j = 0; j < size; j++) {
+          column.push(wholeBoard[j][i]);
+        }
+        columnsArray.push(column);
       }
-      // if (transBoard.hasRowConflictAt(colIndex)) {
-      //   result = true;
-      // }
-      return result; // fixme
+      var transBoard = new Board(columnsArray);
+      return transBoard;
     },
-
-    // _transBoard: function () {
-    //   var wholeBoard = this.rows();
-    //   var size = this.attributes.n;
-    //   var columnsArray = []; 
-    //   for (var i = 0; i < size; i++) { //goes through each row
-    //     //var _currentRow = this.get(i);
-    //     var column = [];
-    //     for (var j = 0; j < size; j++) { //goes through each element in the row
-    //       //columnsArray[j][i] = this[i][j];
-    //       column.push(wholeBoard[j][i])
-    //     }
-    //     columsArray.push(column);
-    //   }
-    //     // create column arrays based off index [0], [1]
-    //     // [1,0,1]   [1,1,0]   board[0][0] board[0][1]
-    //   //   [1,1,1] ->[0,1,1]   board[1][0] board[1][1]
-    //     // [0,1,0]   [1,1,0]   board[2][0] board[2][1]
-    //   var transBoard = new Board({n:size},columnsArray);
-    //   return transBoard;
-    // }
+    hasColConflictAt: function(colIndex) {
+      var transBoard = this._transBoard();
+      return transBoard.hasRowConflictAt(colIndex);
+    },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-
-      return false; // fixme
+      var transBoard = this._transBoard();
+      return transBoard.hasAnyRowConflicts(); // fixme
     },
-
-
 
     // Major Diagonals - go from top-left to bottom-right
     // --------------------------------------------------------------
@@ -169,8 +191,6 @@
     hasAnyMajorDiagonalConflicts: function() {
       return false; // fixme
     },
-
-
 
     // Minor Diagonals - go from top-right to bottom-left
     // --------------------------------------------------------------
@@ -186,8 +206,6 @@
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
-
-
   });
 
   var makeEmptyMatrix = function(n) {
@@ -197,5 +215,4 @@
       });
     });
   };
-
-}());
+})();
